3fe259a54df2ec5d8888901decc7d2d23408a214
Umamaheshwar Burugupalli <Umamaheshwar_Burugupalli@epam.com>
2020-02-07 11:27:44 +0530

    Added new products grid component with service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductsGridComponent } from './products-grid/products-grid.component';
+import { ProductsService } from 'src/Services/productservice';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
